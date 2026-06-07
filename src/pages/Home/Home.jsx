6abfcb6156b15ae6a61b083517136643a16f33ac
Yury Fedulov <buffet2000@gmail.com>
2026-06-07 19:26:44 +0300

    site title changing, some blocks disabling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,8 +12,8 @@ export default function Home() {
       <HelpCards />
       <WhenToAct />
       <Process />
-      <GroupProgram />
-      <IndividualProgram />
+      {/* <GroupProgram /> */}
+      {/* <IndividualProgram /> */}
     </>
   )
 }
